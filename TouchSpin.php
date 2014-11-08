da5e2ffaacdb9c74870b4bb95b34fd1a66e21ea0
Kartik Visweswaran <kartikv2@gmail.com>
2014-11-09 01:53:26 +0530

    Initial release

---
+++ TouchSpin.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
+ * @package yii2-widgets
+ * @subpackage yii2-widget-touchspin
+ * @version 1.0.0
+ */
+
+namespace kartik\touchspin;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+/**
+ * TouchSpin widget is a Yii2 wrapper for the bootstrap-touchspin plugin by
+ * István Ujj-Mészáros. This input widget is a mobile and touch friendly input
+ * spinner component for Bootstrap 3.
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ * @see http://www.virtuosoft.eu/code/bootstrap-touchspin/
+ */
+class TouchSpin extends \kartik\base\InputWidget
+{
+    /**
+     * @var array HTML attributes for the `up` button. The following special
+     * attributes are recognized
+     * - icon: string the glyphicon prefix which will be used for the button label
+     * - label: string the label which will be printed after the icon. This is
+     *   not HTML encoded.
+     */
+    public $upOptions = ['icon' => 'forward', 'class' => 'btn btn-default'];
+
+    /**
+     * @var array HTML attributes for the `down` button. The following special
+     * attributes are recognized
+     * - icon: string the glyphicon prefix which will be used for the button label
+     * - label: string the label which will be printed after the icon. This is
+     *   not HTML encoded.
+     */
+    public $downOptions = ['icon' => 'backward', 'class' => 'btn btn-default'];
+
+    /**
+     * Initializes the widget
+     *
+     * @throw InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        $this->setPluginOptions();
+        $this->registerAssets();
+        echo $this->getInput('textInput');
+    }
+
+    /**
+     * Set the plugin options
+     */
+    protected function setPluginOptions()
+    {
+        $defaults = [];
+        Html::addCssClass($this->upOptions, 'bootstrap-touchspin-up');
+        Html::addCssClass($this->downOptions, 'bootstrap-touchspin-down');
+        $defaults['buttonup'] = $this->renderButton($this->upOptions);
+        $defaults['buttondown'] = $this->renderButton($this->downOptions);
+        $this->pluginOptions = array_replace($defaults, $this->pluginOptions);
+        if (ArrayHelper::getValue($this->pluginOptions, 'verticalbuttons', false) 
+            && empty($this->pluginOptions['prefix'])) {
+            Html::addCssClass($this->options, 'input-left-rounded');
+        }
+    }
+
+    /**
+     * Renders the touchspin button
+     *
+     * @param array $options the button html options
+     * @return string
+     */
+    protected function renderButton($options = [])
+    {
+        if (!empty($this->options['disabled']) && $this->options['disabled'] == true) {
+            $options['disabled'] = true;
+        }
+        $label = ArrayHelper::remove($options, 'label', '');
+        $icon = ArrayHelper::remove($options, 'icon', '');
+        if ($icon != '') {
+            $icon = "<i class='glyphicon glyphicon-{$icon}'></i>";
+            $label = ($label != '') ? $icon . ' ' . $label : $icon;
+        }
+        $options['type'] = 'button';
+        return Html::button($label, $options);
+    }
+
+    /**
+     * Registers the needed assets
+     */
+    public function registerAssets()
+    {
+        $view = $this->getView();
+        $id = 'jQuery("#' . $this->options['id'] . '").parent()';  //unused
+        TouchSpinAsset::register($view);
+        $this->registerPlugin('TouchSpin');
+    }
+}
---
+++ TouchSpinAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
+ * @package yii2-widgets
+ * @subpackage yii2-widget-touchspin
+ * @version 1.0.0
+ */
+
+namespace kartik\touchspin;
+
+/**
+ * Asset bundle for TouchSpin Widget
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class TouchSpinAsset extends \kartik\base\AssetBundle
+{
+
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/assets');
+        $this->setupAssets('css', ['css/bootstrap-touchspin']);
+        $this->setupAssets('js', ['js/bootstrap-touchspin']);
+        parent::init();
+    }
+}
